5df48b34614e39f2387570c85e0551be6d1bffed
Jay <jkns2410@gmail.com>
2022-03-15 02:42:04 +0530

    Frontend|Private Room Btn Complete

--- frontend/src/Components/PrivateModal/PrivateModal.jsx
+++ frontend/src/Components/PrivateModal/PrivateModal.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import TextBox from "../Shared/TextBox/TextBox";
 import styles from './PrivateModal.module.css';
+import { useHistory } from 'react-router-dom';
 
-const PrivateModal = () => {
+const PrivateModal = ({onClose}) => {
+    const history = useHistory();
+    const [link, setLink] = useState('');
+
+    function join() {
+        try{
+            if(!link) return;
+
+            history.push(`/room/${link}`);
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+    
     return (
         <div className={styles.modalMask}> 
             <div className={styles.modalBody}>
+                <button onClick={onClose} className={styles.closeButton}>
+                    <img src="/images/close.png" alt="close"/>
+                </button>
                 <div className={styles.modalHeader}>
                     <h3 className={styles.heading}>Enter Private Invitation Link</h3>
-                    <TextBox fullwidth='true'/>
+                    <TextBox fullwidth='true' value={link} onChange={(e) => setLink(e.target.value)}/>
                 </div>
-                <div className={styles.modalFooter}>
+                <div onClick={join} className={styles.modalFooter}>
                     <button className={styles.footerButton}>
                     <span>Join</span>
                     </button>
--- frontend/src/Pages/Rooms/Rooms.jsx
+++ frontend/src/Pages/Rooms/Rooms.jsx
@@ -63,7 +63,6 @@ const Rooms = () => {
 
     const [showModal, setShowModal] = useState(false);
     const [rooms, setRooms] = useState([]);
-
     const [privateModal, setPrivateModal] = useState(false);
 
     useEffect(() => {
@@ -92,7 +91,7 @@ const Rooms = () => {
                         <input type="text" className={styles.searchInput}/>
                     </div>
                 </div>
-                {/* <div className={styles.roomButtons}> */}
+                <div className={styles.roomButtons}>
                     <div className={styles.right}>
                         <button onClick={openModal} className={styles.startRoomButton}>
                             <img src="/images/add-room-icon.png" alt="add-room"></img>
@@ -100,12 +99,12 @@ const Rooms = () => {
                         </button>
                     </div>
                     <div className={styles.right}>
-                        <button onClick={openPrivateModal} className={styles.startRoomButton}>
+                        <button onClick={openPrivateModal} className={styles.privateRoomButton}>
                             <span> Join Private Room</span>
                         </button>
                     </div>
                     
-                {/* </div> */}
+                </div>
             </div>
             <div className={styles.roomList}>
                 {rooms.map((room) => (            
@@ -114,7 +113,7 @@ const Rooms = () => {
             </div>
         </div>
         {showModal && <AddRoomModal onClose={() => setShowModal(false)}/>}
-        {privateModal && <PrivateModal/>}
+        {privateModal && <PrivateModal onClose={() => setPrivateModal(false)}/>}
     </>
 };
 
